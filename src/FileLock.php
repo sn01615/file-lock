68d0d7ac82e612de60aa8d5b956354b2e87f9ed2
YangLong <168119405@qq.com>
2025-08-24 23:09:54 +0800

    update

--- src/FileLock.php
+++ src/FileLock.php
@@ -5,13 +5,9 @@ namespace PhpUtils;
 class FileLock
 {
 
-    private static $fp;
-
+    private static $fps = [];
     private static $prefix;
-
-    private static $instances = [];
-
-    private $lockFile;
+    private static $instance = null;
 
     public static function lock($tag)
     {
@@ -21,38 +17,51 @@ class FileLock
     /**
      * @param string $tag Unique identification
      * @param bool $die Failed to acquire exclusive lock is whether to execute die()
-     * @return bool If get exclusive lock fail return false, else return true.
+     * @return bool If you get exclusive lock fail return false, else return true.
      */
     public static function getLock($tag, $die = null)
     {
-        if (!self::$prefix) {
-            self::$prefix = sys_get_temp_dir() . '/_PHP_FileLock_';
-        }
+        if (!self::$prefix) self::$prefix = sys_get_temp_dir() . '/_PHP_FileLock_';
+        if (!self::$instance) self::$instance = new static();
 
-        $filename = self::$prefix . sha1($tag);
-        file_put_contents($filename, time());
-        self::$fp = fopen($filename, "r+");
-        if (!flock(self::$fp, LOCK_EX | LOCK_NB)) {
+        $filename = self::getFilename($tag);
+        $fp = fopen($filename, "w");
+        if (!flock($fp, LOCK_EX | LOCK_NB)) {
+            fclose($fp);
             if ($die) die(0);
             return false;
         }
-        self::$instances[$tag] = new static();
-        self::$instances[$tag]->lockFile = $filename;
+        fwrite($fp, time());
+        self::$fps[$filename] = $fp;
         return true;
+    }
+
+    private static function getFilename($tag)
+    {
+        return self::$prefix . sha1($tag);
     }
 
     public static function unlock($tag)
     {
-        if (self::$fp !== null) {
-            flock(self::$fp, LOCK_UN);
+        $filename = self::getFilename($tag);
+        $fp = isset(self::$fps[$filename]) ? self::$fps[$filename] : null;
+        self::clear($fp, $filename);
+    }
+
+    private static function clear($fp, $filename)
+    {
+        if ($fp && is_resource($fp)) {
+            flock($fp, LOCK_UN);
+            fclose($fp);
+            unlink($filename);
+            unset(self::$fps[$filename]);
         }
     }
 
     public function __destruct()
     {
-        if (file_exists($this->lockFile)) {
-            unlink($this->lockFile);
+        foreach (self::$fps as $filename => $fp) {
+            self::clear($fp, $filename);
         }
     }
 }
-
